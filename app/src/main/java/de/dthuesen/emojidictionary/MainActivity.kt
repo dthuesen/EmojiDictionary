8edcfd598c44899753b83c5b740c360492493a5b
Detlef von der Thuesen <dthuesen@gmx.de>
2019-12-17 15:05:38 +0100

    Implement RecyclerView

--- app/src/main/java/de/dthuesen/emojidictionary/MainActivity.kt
+++ app/src/main/java/de/dthuesen/emojidictionary/MainActivity.kt
@@ -2,11 +2,24 @@ package de.dthuesen.emojidictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var layoutManager : GridLayoutManager
+    lateinit var adapter: EmojiAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutManager = GridLayoutManager(this, 6)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = EmojiAdapter(arrayListOf("💫","🌊","🎂","🚂","🛸","👨‍💻","👨‍🎤‍","👾","🤠"))
+        recyclerView.adapter = adapter
     }
 }
